abce6cb62a3c3217220278a25a8df0e2beed8f8b
Tung Dam <damtung2000@gmail.com>
2023-03-24 22:24:28 +0900

    add button

--- functions/thankYouBolt.js
+++ functions/thankYouBolt.js
@@ -14,15 +14,28 @@ const app = new App({
 
 app.message('hello', async ({ message, say }) => {
   // say() sends a message to the channel where the event was triggered
-  await say(`Hey there <@${message.user}>!`);
+  await say({
+    blocks: [
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `Hey there <@${message.user}>!`,
+        },
+        accessory: {
+          type: 'button',
+          text: {
+            type: 'plain_text',
+            text: 'Click Me',
+          },
+          action_id: 'button_click',
+        },
+      },
+    ],
+    text: `Hey there <@${message.user}>!`,
+  });
 });
 
-// (async () => {
-//   // Start your app
-//   await app.start(process.env.PORT || 3000);
-
-//   console.log('⚡️ Bolt app is running!');
-// })();
 exports.handler = async (event, context) => {
   const payload = parseRequestBody(event.body, event.headers['content-type']);
 
